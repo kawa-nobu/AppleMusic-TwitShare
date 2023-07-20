7e4883d8ec6533746b94d0632d22b69b6cb8229c
kawa-nobu <44832116+kawa-nobu@users.noreply.github.com>
2023-07-20 22:13:13 +0900

    色々修正
    稀にアーティスト名が取れなかったりしていたので取得先のタグを変更した。あと、色々項目増やした。

--- index.php
+++ index.php
@@ -7,25 +7,32 @@ if(parse_url($_GET['url'])['scheme'] == "https"){
         $dom = new DOMDocument('1.0', 'UTF-8');
         @$dom->loadHTML($apple_link);
         $domxpath = new DOMXpath($dom);
-        $song_json = json_decode($domxpath->query("//script")->item(0)->nodeValue, true);
+        //$song_json = json_decode($domxpath->query("//script")->item(0)->nodeValue, true);
+        $song_json = json_decode($domxpath->query('//*[@id="serialized-server-data"]')->item(0)->nodeValue, true);
+        //echo $domxpath->query('//*[@id="serialized-server-data"]')->item(0)->nodeValue;
+        //echo $song_json[0]['data']['seoData']['ogSongs'][0]['attributes']['name'];
+        $lbart_url = str_replace(array('{w}', '{h}', '{f}'), array('99999', '99999', 'jpg'), $song_json[0]['data']['seoData']['ogSongs'][0]['attributes']['artwork']['url']);
+        $release_date = date('Y年m月d日',strtotime($song_json[0]['data']['seoData']['ogSongs'][0]['attributes']['releaseDate']));
         echo "
         <html>
             <head>
-                <title>{$song_json['name']}-AppleMusic</title>
+                <title>{$song_json[0]['data']['seoData']['ogSongs'][0]['attributes']['name']}-AppleMusic</title>
                 <meta name=\"twitter:card\" content=\"summary\" />
                 <meta name=\"twitter:site\" content=\"@kw_nobu\" />
                 <meta name=\"twitter:creator\" content=\"@kw_nobu\" />
-                <meta property=\"og:url\" content=\"{$song_json['url']}\" />
-                <meta property=\"og:title\" content=\"「{$song_json['name']}」をAppleMusicで聴く\" />
-                <meta property=\"og:description\" content=\"アーティスト:{$song_json['byArtist'][0]['name']} Link Created by kawa-nobu\" />
-                <meta property=\"og:image\" content=\"{$song_json['image']}\" />
+                <meta property=\"og:url\" content=\"{$song_json[0]['data']['seoData']['ogSongs'][0]['attributes']['url']}\" />
+                <meta property=\"og:title\" content=\"「{$song_json[0]['data']['seoData']['ogSongs'][0]['attributes']['name']}」をAppleMusicで聴く\" />
+                <meta property=\"og:description\" content=\"今すぐ「{$song_json[0]['data']['seoData']['ogSongs'][0]['attributes']['artistName']}」の「{$song_json[0]['data']['seoData']['ogSongs'][0]['attributes']['name']}」を聴こう!\" />
+                <meta property=\"og:image\" content=\"{$lbart_url}\" />
                 <style>body{text-align: center;}</style>
             </head>
             <body>
-                <h1>{$song_json['name']} - {$song_json['byArtist'][0]['name']}</h1>
-                <p><a href=\"{$song_json['url']}\" target=\"_blank\" rel=\"noopener noreferrer\">AppleMusicを開く</a></p>
-                <div><img src=\"{$song_json['image']}\"></div>
-                <div><audio controls src=\"{$song_json['audio']['contentUrl']}\"></div>
+                <h1>{$song_json[0]['data']['seoData']['ogSongs'][0]['attributes']['name']} - {$song_json[0]['data']['seoData']['ogSongs'][0]['attributes']['artistName']}</h1>
+                <p><a href=\"{$song_json[0]['data']['seoData']['url']}\" target=\"_blank\" rel=\"noopener noreferrer\">{$song_json[0]['data']['seoData']['ogSongs'][0]['attributes']['albumName']}</a>({$release_date}リリース)</p>
+                <p>作曲者:{$song_json[0]['data']['seoData']['ogSongs'][0]['attributes']['composerName']}(<a href=\"https://ja.wikipedia.org/wiki/{$song_json[0]['data']['seoData']['ogSongs'][0]['attributes']['composerName']}\" target=\"_blank\" rel=\"noopener noreferrer\">ウィキペディア</a>)</p>
+                <p><a href=\"{$song_json[0]['data']['seoData']['ogSongs'][0]['attributes']['url']}\" target=\"_blank\" rel=\"noopener noreferrer\">AppleMusicを開く</a></p>
+                <div><img src=\"{$lbart_url}\" height=\"65%\"></div>
+                <div><audio controls src=\"{$song_json[0]['data']['seoData']['ogSongs'][0]['attributes']['previews'][0]['url']}\"></div>
                 <div><p>Link Created by kawa-nobu <a href=\"https://github.com/kawa-nobu/AppleMusic-TwitShare\" target=\"_blank\" rel=\"noopener noreferrer\">GitHub</a></p></div>
             </body>
         </html>
